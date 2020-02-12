30197f006fcdefc07993a60604fa93194dc894f8
anzihan <13991821330@163.com>
2020-02-12 12:29:40 +0800

    最长无重复子串

--- src/array/SlideWindow2.java
+++ src/array/SlideWindow2.java
@@ -1,4 +1,25 @@
 package array;
 
 public class SlideWindow2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int getResult(int[] arr) {
+        int l = 0;
+        int r = 0; //[l,r]是无重复最长子串
+        int[] freq = new int[256];
+        int result = 1;
+        while (l < arr.length) {
+            if (freq[arr[r]] == 0) {
+                result = Math.max(result, r - l + 1);
+                freq[arr[r++]] ++;
+            } else {
+                freq[arr[l++]] --;
+            }
+        }
+        return result;
+    }
+
 }
